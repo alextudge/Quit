2ffbdad29313db3cf3e123f910b2f2a8b48a02a6
Alex Tudge <alextudge@hotmail.co.uk>
2018-12-27 19:25:58 +0000

    editarrayVC UI

--- Quit/Controller/EditArrayVC.swift
+++ Quit/Controller/EditArrayVC.swift
@@ -9,19 +9,36 @@
 import UIKit
 
 class EditArrayVC: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var editArrayTextView: UITextView!
     
     var persistenceManager: PersistenceManager?
     var isReasonsToSmoke = false
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
     @IBAction func didTapSaveButton(_ sender: Any) {
-        
+        dismiss(animated: true, completion: nil)
     }
 }
 
 private extension EditArrayVC {
+    func setupUI() {
+        editArrayTextView.layer.cornerRadius = 10
+        editArrayTextView.layer.borderWidth = 2
+        editArrayTextView.layer.borderColor = UIColor.white.cgColor
+        if isReasonsToSmoke {
+            editArrayTextView.text = arrayToTextBlock(array: persistenceManager?.additionalUserData?.reasonsToSmoke ?? [])
+        } else {
+            editArrayTextView.text = arrayToTextBlock(array: persistenceManager?.additionalUserData?.reasonsNotToSmoke ?? [])
+        }
+        editArrayTextView.becomeFirstResponder()
+    }
+    
     func textToArray(text: String) -> [String] {
         return text.components(separatedBy: [","])
     }
